e7b914bd1f97649a0188cfb8f253a64c231bac4e
mrBod1 <mistryanubogdan@gmail.com>
2024-03-04 21:04:53 +0100

    Lessons 2.2

--- js/task-4.js
+++ js/task-4.js
@@ -1,16 +1,20 @@
 function getShippingCost(country) {
-    let price = 250;
+    let price = ` `;
     switch (country) {
         case `China`:
-            return `Shipping to ${country} will cost ${price} credits`;
+             price = `100`;
+           return `Shipping to ${country} will cost ${price} credits`; 
             break;
         case `Chile`:
+             price = `250`;
             return `Shipping to ${country} will cost ${price} credits`;
             break;
         case `Australia`:
+             price = `170`;
             return `Shipping to ${country} will cost ${price} credits`;
             break;
         case `Jamaica`:
+             price = `120`;
             return `Shipping to ${country} will cost ${price} credits`;
             break;
         default:
